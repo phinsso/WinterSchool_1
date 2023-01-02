bcadf683988e97f5fe4cdb1a9b78d319066c7a7c
phinsso <naso01247@gmail.com>
2023-01-02 15:30:48 +0900

    vending machine(GUI) MVC Model

--- src/vendingmachine/controller/ProductController.java
+++ src/vendingmachine/controller/ProductController.java
@@ -1,7 +1,8 @@
 package vendingmachine.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -22,20 +23,22 @@ public class ProductController extends JFrame {
 		ProductView view = new ProductView();
 		fullProduct();
 		view.setProductList(pvoList);
-		JPanel panC = view.displayProducts();
+		JPanel panC = view.displayProducts(this);
+		JPanel panS = view.inputPurchase();
 		add(panC, "Center");
+		add(panS, "South");
 		
 		setTitle("음료 자판기");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setBounds(100, 50, 600, 600); //setLocation + setSize
+		setBounds(100, 20, 600, 750); //setLocation + setSize
 		setVisible(true);
 	}
 	
 	//제품 채우기
 	public void fullProduct() {
 		ProductDAO dao = new ProductDAO();
-		String[] names = {"사이다", "콜라", "백포도웰치스", "적포도웰치스", "제로콜라", "초록매실", "오렌지쥬스", "모구모구", "박카스"};
-		String[] images = {"cider", "cola", "welchs_white", "welchs_grape", "zerocola", "choroc", "orange", "mogumogu", "f"};
+		String[] names = {"사이다", "콜라", "초코우유", "적포도웰치스", "토레타", "초록매실", "오렌지쥬스", "모구모구", "박카스"};
+		String[] images = {"cider", "cola", "chocomilk", "welchs_grape", "toleta", "choroc", "orange", "mogumogu", "f"};
 		int[] prices = {1200, 1200, 1000, 1000, 1400, 700, 800, 1500, 900};
 		ProductVO pvo = null;
 		pvoList = dao.select();
