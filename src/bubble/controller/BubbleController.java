490b92ec504900458e5386b9984c4fd0dffe7401
phinsso <naso01247@gmail.com>
2023-01-06 15:29:48 +0900

    bubbletea making game (Using Thread)

---
+++ src/bubble/controller/BubbleController.java
@@ -0,0 +1,11 @@
+package bubble.controller;
+
+import bubble.view.BubbleStart;
+
+public class BubbleController {
+
+	public static void main(String[] args) {
+		new BubbleStart();
+	}
+
+}
---
+++ src/bubble/vo/Pearl.java
@@ -0,0 +1,46 @@
+package bubble.vo;
+
+public class Pearl {
+	
+	private int x, y;
+	private int w, h;
+	private String imgName;
+	private int point;
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getW() {
+		return w;
+	}
+	public void setW(int w) {
+		this.w = w;
+	}
+	public int getH() {
+		return h;
+	}
+	public void setH(int h) {
+		this.h = h;
+	}
+	public String getImgName() {
+		return imgName;
+	}
+	public void setImgName(String imgName) {
+		this.imgName = imgName;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+}
